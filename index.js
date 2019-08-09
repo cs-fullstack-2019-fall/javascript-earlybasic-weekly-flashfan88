e95109e19ff70504f6fbc7277d2bf906b220554b
flashfan88 <lavarjohnson20@gmail.com>
2019-08-09 16:54:40 -0500

    ireallytried

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 
-var userInput1 = parseInt(prompt("Welcome to LaVar's Corner. Press the 1 key to count. Press the 2 key to use Calculator. Press the E key to exit."));
-var userInput2 = 1000;
+// var userInput1 = parseInt(prompt("Welcome to LaVar's Corner. Press the 1 key to count. Press the 2 key to use Calculator. Press the E key to exit."));
+var number = 0;
+
+
+
+while (number <1000)
+{
+	console.log(number);
+	number = number +1;
+}
+
 if (userInput1 ===1)
 {
 
 	prompt("Press 1 to count by 1's. Press 2 to count by 2's")
-	console.log()
+	if (userInput1 ===1) 
+	{
+
+	}
 }
 if (userInput1 ===2)
 {
 	prompt("Would you like to use Calculator? Press Y for Yes or N for No");
 }
-if (userInput1 ===E)
-{
-	alert("Goodbye");
-}
+
 If (userInput1 === Y)
 {
 	Alert("You are now using the calculator");
